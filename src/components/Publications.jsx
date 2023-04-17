f2be6d2be0cbbc79c8c042e9e52ba2094ac43173
Pulak <pulakkumarghosh2001@gmail.com>
2023-04-17 16:38:47 +0530

    Publications

--- src/components/Publications.jsx
+++ src/components/Publications.jsx
@@ -1,11 +1,51 @@
 import React, { Component } from 'react'
 import Heading from './Heading'
+import axios from 'axios'
+import './Publications.css';
 
 export class Publications extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      publications: [],
+    }
+  }
+  async componentDidMount() {
+    const url = "https://script.google.com/macros/s/AKfycbxATDI60mhkONYhLRv9na5ODgA8FVzXDLjkESNKFklD0z_K3RL4g9OTqzAjY2h_rnr4-g/exec?page=publications";
+    const response = await axios.get(url);
+    this.setState({publications: response.data.data});
+  }
   render() {
     return (
       <div>
         <Heading title="Publications" />
+        <div className="m-5 d-flex flex-column">
+          {this.state.publications.length === 0 && (
+            <div className="loader-container">
+              <div className="spinner"></div>
+            </div>
+          )}
+          {this.state.publications.length !== 0 && <table className="table table-borderless">
+            <thead>
+              <tr>
+                <th scope="col">Authors</th>
+                <th scope="col">Title</th>
+                <th scope="col">Link</th>
+                <th scope="col">Year</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.publications.map((i, index) =>(
+                <tr key={index}>
+                  <td>{i.Authors}</td>
+                  <td>{i.Title}</td>
+                  <td><a target="_blank" href={`${i.Link}`}>Click here</a></td>
+                  <td>{i.Year}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>}
+        </div>
       </div>
     )
   }
--- src/components/Research.jsx
+++ src/components/Research.jsx
@@ -6,6 +6,19 @@ export class Research extends Component {
     return (
       <div>
         <Heading title="Research" />
+        <div className="m-4 d-flex flex-column justify-content-center">
+          <h4>Research Area</h4>
+          <ul>
+            <li>Mathematical Modelling on disease Psoriasis & AIDS</li>
+            <li>Ecology</li>
+            <li>Epidermology</li>
+          </ul>
+          <h4>Research Experiences</h4>
+          <ul>
+            <li>Agricultural and Ecological Research Unit, Indian Statistical Institute, Kolkata, India from 1996 to 2007.</li>
+            <li>Current area of Research is Mathematical Modeling on Ecology with special emphasis on transmissible disease, modeling on disease Psoriasis, HIV, Enzyme Kinetics, Biodiesel Production. </li>
+          </ul>
+        </div>
       </div>
     )
   }
