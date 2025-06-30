42df6cf2e38b28f5e283a5aa97120467b7c4ad37
Satyajitju <141834113+Satyajitju@users.noreply.github.com>
2025-06-30 22:41:05 +0530

    Update Students.jsx

--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -12,66 +12,100 @@ export class Students extends Component {
       loading: false,
     }
   }
-  async componentDidMount(){
-    this.setState({loading: true});
+
+  async componentDidMount() {
+    this.setState({ loading: true });
     const url = "https://script.google.com/macros/s/AKfycbxATDI60mhkONYhLRv9na5ODgA8FVzXDLjkESNKFklD0z_K3RL4g9OTqzAjY2h_rnr4-g/exec?page=students";
     const response = await axios.get(url);
-    this.setState({students: response.data.data, loading: false});
+    this.setState({ students: response.data.data, loading: false });
   }
-  async courseChangeToMsc () {
-    this.setState({type: 'msc', loading: true, students: []});
-    const url = "https://script.google.com/macros/s/AKfycbxATDI60mhkONYhLRv9na5ODgA8FVzXDLjkESNKFklD0z_K3RL4g9OTqzAjY2h_rnr4-g/exec?page=msc";
-    const response = await axios.get(url);
-    this.setState({students: response.data.data, loading: false});
-  }
-  courseChangeToPhd = async()=> {
-    this.setState({type: 'phd', loading: true, students: []});
+
+  courseChangeToPhd = async () => {
+    this.setState({ type: 'phd', loading: true, students: [] });
     const url = "https://script.google.com/macros/s/AKfycbxATDI60mhkONYhLRv9na5ODgA8FVzXDLjkESNKFklD0z_K3RL4g9OTqzAjY2h_rnr4-g/exec?page=students";
     const response = await axios.get(url);
-    this.setState({students: response.data.data, loading: false});
+    this.setState({ students: response.data.data, loading: false });
   }
-  courseChangeToCurrentPhd = async()=> {
-    this.setState({type: 'current_phd', loading: true, students: []});
+
+  courseChangeToCurrentPhd = async () => {
+    this.setState({ type: 'current_phd', loading: true, students: [] });
     const url = "https://script.google.com/macros/s/AKfycbxATDI60mhkONYhLRv9na5ODgA8FVzXDLjkESNKFklD0z_K3RL4g9OTqzAjY2h_rnr4-g/exec?page=Current_phd";
     const response = await axios.get(url);
-    this.setState({students: response.data.data, loading: false});
+    this.setState({ students: response.data.data, loading: false });
   }
+
+  courseChangeToMsc = async () => {
+    this.setState({ type: 'msc', loading: true, students: [] });
+    const url = "https://script.google.com/macros/s/AKfycbxATDI60mhkONYhLRv9na5ODgA8FVzXDLjkESNKFklD0z_K3RL4g9OTqzAjY2h_rnr4-g/exec?page=msc";
+    const response = await axios.get(url);
+    this.setState({ students: response.data.data, loading: false });
+  }
+
+  courseChangeToPostDoc = async () => {
+    this.setState({ type: 'post_doc', loading: true, students: [] });
+    const url = "https://script.google.com/macros/s/AKfycbxATDI60mhkONYhLRv9na5ODgA8FVzXDLjkESNKFklD0z_K3RL4g9OTqzAjY2h_rnr4-g/exec?page=post_doc";
+    const response = await axios.get(url);
+    this.setState({ students: response.data.data, loading: false });
+  }
+
   render() {
     return (
       <div>
         <Heading title="Students" />
-        <div className="d-flex flex-row justify-content-around m-3">
-                  <div onClick={()=> this.courseChangeToPhd()} className={`col-3 text-center border border-1 border-danger rounded-2 m-2 p-2 ${this.state.type === 'phd' ? "bg-danger text-light" : 'text-danger'}`} style={{cursor: 'pointer'}}>Awarded PhD Students</div>
-                  <div onClick={()=> this.courseChangeToCurrentPhd()} className={`col-3 text-center border border-1 border-danger rounded-2 m-2 p-2 ${this.state.type === 'current_phd' ? "bg-danger text-light" : 'text-danger'}`} style={{cursor: 'pointer'}}>Current PhD Students</div>
-                  <div onClick={()=> this.courseChangeToMsc()} className={`col-3 text-center border border-1 border-danger rounded-2 m-2 p-2 ${this.state.type === 'msc' ? "bg-danger text-light" : "text-danger"}`} style={{cursor: 'pointer'}}>MSc Students</div>
+        <div className="d-flex flex-row justify-content-around m-3 flex-wrap">
+          <div onClick={this.courseChangeToPhd} className={`col-3 text-center border border-1 border-danger rounded-2 m-2 p-2 ${this.state.type === 'phd' ? "bg-danger text-light" : 'text-danger'}`} style={{ cursor: 'pointer' }}>
+            Awarded PhD Students
+          </div>
+          <div onClick={this.courseChangeToCurrentPhd} className={`col-3 text-center border border-1 border-danger rounded-2 m-2 p-2 ${this.state.type === 'current_phd' ? "bg-danger text-light" : 'text-danger'}`} style={{ cursor: 'pointer' }}>
+            Current PhD Students
+          </div>
+          <div onClick={this.courseChangeToMsc} className={`col-3 text-center border border-1 border-danger rounded-2 m-2 p-2 ${this.state.type === 'msc' ? "bg-danger text-light" : "text-danger"}`} style={{ cursor: 'pointer' }}>
+            MSc Students
+          </div>
+          <div onClick={this.courseChangeToPostDoc} className={`col-3 text-center border border-1 border-danger rounded-2 m-2 p-2 ${this.state.type === 'post_doc' ? "bg-danger text-light" : "text-danger"}`} style={{ cursor: 'pointer' }}>
+            Postdoc Students
+          </div>
         </div>
+
         <div className="m-5 d-flex flex-column">
           {this.state.loading && (
             <div className="loader-container">
               <div className="spinner"></div>
             </div>
           )}
-          {this.state.students.length !== 0 && <table className="table table-borderless">
-            <thead>
-              <tr>
-                <th scope="col">Student</th>
-                <th scope="col">{this.state.type === 'msc' ? 'Title of Project':'Title of Thesis'}</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.students.map((i, index) =>(
-                <tr key={index}>
-                  <td>{i.Name}</td>
-                  <td>{i.Thesis}</td>
-                  <td>{i.Project}</td>
-            
+          {this.state.students.length !== 0 &&
+            <table className="table table-borderless">
+              <thead>
+                <tr>
+                  <th scope="col">Student</th>
+                  <th scope="col">
+                    {this.state.type === 'msc'
+                      ? 'Title of Project'
+                      : this.state.type === 'post_doc'
+                      ? 'Research Topic'
+                      : 'Title of Thesis'}
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>}
-          {this.state.students.length === 0 &&
-            <div className="loader-container">
-              <div className="spinner"></div>
+              </thead>
+              <tbody>
+                {this.state.students.map((i, index) => (
+                  <tr key={index}>
+                    <td>{i.Name}</td>
+                    <td>
+                      {this.state.type === 'msc'
+                        ? i.Project
+                        : this.state.type === 'post_doc'
+                        ? i.Topic
+                        : i.Thesis}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          }
+          {this.state.students.length === 0 && !this.state.loading &&
+            <div className="text-center mt-3 text-muted">
+              No data available for this category.
             </div>
           }
         </div>
@@ -80,4 +114,4 @@ export class Students extends Component {
   }
 }
 
-export default Students
+export default Students;
