fada9201a03aacc610356f64e079ae83359d37d6
Satyajitju <141834113+Satyajitju@users.noreply.github.com>
2025-07-01 01:35:12 +0530

    Update Students.jsx

--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Heading from './Heading';
 import axios from 'axios';
-import './Publications.css';
+import './Students.css';
 
 export class Students extends Component {
   constructor(props) {
